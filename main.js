7546069fe7ba9bf3a0a9949b8946a658fbdf6efd
velitmike <58567671+velitmike@users.noreply.github.com>
2021-06-24 20:31:08 +1200

    Добавили столкновения машин, результат гонки

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict";
 const score = document.querySelector(".score"),
   start = document.querySelector(".start"),
   gameArea = document.querySelector(".gameArea"),
@@ -28,6 +29,7 @@ function getQuantityElements(heightEliment) {
 }
 function startGame() {
   start.classList.add("hide");
+  gameArea.innerHTML = "";
 
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement("div");
@@ -36,6 +38,7 @@ function startGame() {
     line.y = i * 100;
     gameArea.appendChild(line);
   }
+
   for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
     const enemy = document.createElement("div");
     enemy.classList.add("enemy");
@@ -47,8 +50,13 @@ function startGame() {
       "transparent url(./image/enemy.png) center / cover no-repeat";
     gameArea.appendChild(enemy);
   }
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  // car.style.left = "125px";
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+  car.style.top = "auto";
+  car.style.bottom = "10px";
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -57,6 +65,9 @@ function playGame() {
   // console.log("Play Game");
 
   if (setting.start === true) {
+    setting.score += setting.speed;
+    // score.textContent = "SCORE: " + setting.score;
+    score.innerHTML = "SCORE<br>" + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -103,6 +114,19 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll(".enemy");
   enemy.forEach(function (item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+    if (
+      carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top
+    ) {
+      setting.start = false;
+      console.warn("ДТП");
+      start.classList.remove("hide");
+      start.style.top = score.offsetHeight;
+    }
     item.y += setting.speed / 2;
     item.style.top = item.y + "px";
     if (item.y > document.documentElement.clientHeight) {
